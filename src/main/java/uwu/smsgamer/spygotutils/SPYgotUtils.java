88d827b0350a94d0577d6b8adbf25c7c43f20021
true-cc <creepercannyon@gmail.com>
2021-03-01 10:39:53 -0800

    Forgot to generate test-config.py

--- src/main/java/uwu/smsgamer/spygotutils/SPYgotUtils.java
+++ src/main/java/uwu/smsgamer/spygotutils/SPYgotUtils.java
@@ -69,6 +69,7 @@ public final class SPYgotUtils {
 
     private void aScriptFiles() {
         FileUtils.saveResource(getLoader(), "all/test.py", new File(getLoader().getDataFolder(), "scripts/test.py"), false);
+        FileUtils.saveResource(getLoader(), "all/test-conf.py", new File(getLoader().getDataFolder(), "scripts/itest.py"), false);
         FileUtils.saveResource(getLoader(), "all/itest.py", new File(getLoader().getDataFolder(), "scripts/itest.py"), false);
         FileUtils.saveResource(getLoader(), "all/sql.py", new File(getLoader().getDataFolder(), "scripts/sql.py"), false);
     }
