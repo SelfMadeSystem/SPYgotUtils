3af11e84485a07d1f4b34fe658891e26b1851360
sms <creepercannyon@gmail.com>
2021-04-23 16:05:59 -0700

    Attempt idiot fix

--- src/main/java/uwu/smsgamer/spygotutils/listener/spigot/SPacketProcessor.java
+++ src/main/java/uwu/smsgamer/spygotutils/listener/spigot/SPacketProcessor.java
@@ -5,6 +5,7 @@ import io.github.retrooper.packetevents.event.impl.PacketPlaySendEvent;
 import io.github.retrooper.packetevents.packettype.PacketType;
 import io.github.retrooper.packetevents.packetwrappers.NMSPacket;
 import io.github.retrooper.packetevents.packetwrappers.play.out.chat.WrappedPacketOutChat;
+import org.bukkit.Bukkit;
 import uwu.smsgamer.spygotutils.managers.*;
 
 public class SPacketProcessor extends PacketListenerAbstract {
@@ -12,7 +13,8 @@ public class SPacketProcessor extends PacketListenerAbstract {
     public void onPacketPlaySend(PacketPlaySendEvent event) {
         if (event.getPacketId() == PacketType.Play.Server.CHAT) {
             WrappedPacketOutChat packet = new WrappedPacketOutChat(event.getNMSPacket());
-            boolean check = true;
+            boolean check = !(Bukkit.getVersion().contains("1.13") || Bukkit.getVersion().contains("1.14") ||
+                    Bukkit.getVersion().contains("1.15") || Bukkit.getVersion().contains("1.16"));
             try {
                 check = packet.getChatPosition().equals(WrappedPacketOutChat.ChatPosition.CHAT);
             } catch (IllegalArgumentException ignored) {
